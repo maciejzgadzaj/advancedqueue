4497fae57ac34e4b0e236aa715f70e65ec0c961d
Maciej Zgadzaj <maciej.zgadzaj@gmail.com>
2017-02-24 17:49:40 -0500

    Example module

---
+++ modules/advancedqueue_example/src/Plugin/QueueWorker/UnluckyQueue.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\advancedqueue_example\Plugin\QueueWorker;
+
+use Drupal\Core\Queue\QueueWorkerBase;
+use Drupal\Core\Queue\RequeueException;
+use Drupal\Core\Queue\SuspendQueueException;
+
+/**
+ * An example of basic AdvancedQueue queue worker definition.
+ *
+ * @QueueWorker(
+ *   id = "unlucky_queue",
+ *   title = @Translation("Unlucky queue"),
+ *   description = @Translation("A queue which will most probably fail processing items."),
+ *   group = "Examples",
+ *   retry = {
+ *     "attempts" = 10,
+ *     "delay" = 30,
+ *   },
+ * )
+ *
+ * @see AdvancedQueueWorkerManager::$defaults
+ *   for more information on all available properties and their values.
+ * @see \Drupal\advancedqueue_example\Plugin\QueueWorker\LuckyQueue
+ *   for full definition example.
+ */
+class UnluckyQueue extends QueueWorkerBase {
+
+  /**
+   * {@inheritdoc}
+   *
+   * @see \Drupal\advancedqueue_example\Plugin\QueueWorker\LuckyQueue
+   *   for more information on additional parameters and processing flow.
+   */
+  public function processItem($data) {
+    \Drupal::logger('advancedqueue_example')->debug(t('The "@name" worker is now processing item.', ['@name' => $this->getPluginId()]));
+
+    sleep(5);
+
+    $result = mt_rand(0, 10);
+    if ($result < 3) {
+      return 'Processed successfully.';
+    }
+    elseif ($result < 7) {
+      throw new RequeueException('Processing failed, will retry.');
+    }
+    elseif ($result < 8) {
+      throw new SuspendQueueException('Queue processing suspended.');
+    }
+    else {
+      throw new \Exception('Processing failed.');
+    }
+  }
+
+}
